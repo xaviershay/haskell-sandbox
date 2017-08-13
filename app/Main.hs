9cc0fc9c5937f202e8b891bf44a29267116481f2
Xavier Shay <contact@xaviershay.com>
2017-08-12 17:46:34 -0700

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -5,19 +5,25 @@ import           Text.Parsec            hiding ( optional)
 import           Text.Parsec.Expr
 import qualified Data.Vector            as V
 import qualified Data.Map               as M
+import Data.Maybe (fromJust)
 
 type UserString = String
 
-data Token = CommandToken Command | Comment String | Whitespace | Label String | Variable String
+data Token = CommandToken Command | Comment String | Whitespace | Label String
 
 data Command =
   PrintByte |
   ReadNum |
+  Dup |
+  PushVar String |
+  StoreVar String |
+  Call |
+  Jump |
+  Noop |
   Exit
   deriving (Show)
 
 data Program = Program {
-  labelMap :: M.Map String Int,
   commands :: V.Vector Command,
   stack :: [Int],
   variables :: M.Map String Int,
@@ -32,10 +38,53 @@ main = do
 
   case parse17 contents of
     Left x -> putStrLn $ show x
-    Right x -> putStrLn . show $ x
+    Right x -> do
+      putStrLn $ show x
+      eval x
 
 
-evalCommand p (Command Exit) = p { instructionPointer = V.length (commands p) }
+eval :: Program -> IO ()
+eval p = do
+  case commands p V.!? instructionPointer p of
+    Nothing -> do
+      putStrLn ""
+      putStrLn . show $ p
+      return ()
+    Just c -> do
+      putStrLn . show $ c
+      p' <- evalCommand p { instructionPointer = instructionPointer p + 1 } c
+      eval p'
+
+evalCommand p Noop = return p
+evalCommand p ReadNum = return $ p { stack = 42:stack p }
+evalCommand p PrintByte = do
+  let (byte:stack') = stack p
+  putStr . show $ byte
+  return p { stack = stack' }
+
+evalCommand p Exit = return $ p { instructionPointer = V.length (commands p) }
+--evalCommand p Dup = return $ p { stack = top:s }
+--  where
+--    s = stack p
+--    top = head s
+
+evalCommand p (StoreVar x) = return $ p { variables = M.insert x top (variables p), stack = stack' }
+  where
+    (top:stack') = stack p
+
+evalCommand p (PushVar x) = return $ p { stack = value:stack p}
+  where
+    value = case M.lookup x (variables p) of
+      Nothing -> error ("Failed to lookup: " ++ x)
+      Just x  -> x
+
+evalCommand p Call = return $ p { instructionPointer = location, stack = (instructionPointer p):stack'}
+  where
+    (location:stack') = stack p
+
+evalCommand p Jump = return $ p { instructionPointer = location, stack = stack' }
+  where
+    (location:stack') = stack p
 
 parse17 :: UserString -> ParseResult
 parse17 input = runParser xxx () input input
@@ -47,9 +96,17 @@ xxx = do
   stream <- many (comment <|> try label17 <|> command <|> variable <|> whitespace)
 
   return Program {
-    labelMap = filterLabels 0 M.empty stream,
-    commands = V.fromList (filterCommands stream)
+    variables = filterLabels 0 M.empty stream,
+    commands = V.fromList (identifyStores . filterCommands $ stream),
+    stack = [],
+    instructionPointer = 0
   }
+
+identifyStores :: [Command] -> [Command]
+identifyStores (PushVar x:StoreVar y:cs)= StoreVar x:Noop:identifyStores cs
+identifyStores (StoreVar y:cs) = undefined -- parse error
+identifyStores (c:cs) = c:identifyStores cs
+identifyStores [] = []
 
 filterLabels :: Int -> M.Map String Int -> [Token] -> M.Map String Int
 filterLabels i m ((CommandToken c):xs) = filterLabels (i+1) m xs
@@ -81,6 +138,10 @@ command = do
       makeCommand "print_byte" PrintByte
   <|> makeCommand "read_num" ReadNum
   <|> makeCommand "exit" Exit
+  <|> makeCommand "dup" Dup
+  <|> makeCommand "jump" Jump
+  <|> makeCommand "call" Call
+  <|> makeCommand "store" (StoreVar "")
 
 makeCommand x t = do
   try (string x)
@@ -89,4 +150,4 @@ makeCommand x t = do
  
 variable = do
   x <- many1 alphaNum
-  return $ Variable x
+  return $ CommandToken (PushVar x)
