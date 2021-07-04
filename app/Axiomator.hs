5503f21ef5fb052e01bcf505c5474fa5bcadce5a
Xavier Shay <contact@xaviershay.com>
2021-07-04 12:20:57 +1000

    Making progress on series

--- app/Axiomator.hs
+++ app/Axiomator.hs
@@ -106,7 +106,23 @@ axiomMultiplyConst = Axiom {
 }
   where
     f (Product (Const a) (Const b)) = Right (Const $ a * b)
+    f (Exponent (Const a) (Const b)) = Right (Const $ a ^ b)
     f t = Left t
+
+axiomFactorialConst = Axiom {
+  description = "Factorial constants",
+  example = (
+    Factorial (Const 3),
+    (Const 6)
+  ),
+  implementation = f
+}
+  where
+    f (Factorial (Const x)) = Right . Const $ factorial x
+    f t = Left t
+
+factorial 0 = 1
+factorial x = x * factorial (x-1)
 
 axiomIdentitySum = Axiom {
   description = "Additive identity",
@@ -132,6 +148,19 @@ axiomIdentityProduct = Axiom {
   where
     f (Product (Const 1) t) = Right t
     f (Product t (Const 1)) = Right t
+    f (Fraction t (Const 1)) = Right t
+    f t = Left t
+
+axiomNullExponent = Axiom {
+  description = "Exponent of 0",
+  example = (
+    (Exponent (Var "a") (Const 0)),
+    (Const 1)
+  ),
+  implementation = f
+}
+  where
+    f (Exponent t (Const 0)) = Right (Const 1)
     f t = Left t
 
 axiomIdentity = Axiom {
@@ -186,10 +215,12 @@ allAxioms =
   , axiomAssociateProduct
   , axiomIdentitySum
   , axiomIdentityProduct
+  , axiomNullExponent
   , axiomDistribute
   , axiomStepSeries
   , axiomSumConst
   , axiomMultiplyConst
+  , axiomFactorialConst
   ]
 
 data Term =
@@ -197,46 +228,74 @@ data Term =
   Sum Term Term |
   Product Term Term |
   Var String |
-  Series String Term Term
+  Series String Term Term |
+  Factorial Term |
+  Fraction Term Term |
+  Exponent Term Term
   deriving (Show, Eq)
 
 walk :: (Term -> Term) -> Term -> Term
 walk f (Sum a b) = f (Sum (walk f a) (walk f b))
 walk f (Product a b) = f (Product (walk f a) (walk f b))
 walk f (Series v i t) = f (Series v (walk f i) (walk f t))
+walk f (Factorial t) = f (Factorial (walk f t))
+walk f (Fraction a b) = f (Fraction (walk f a) (walk f b))
+walk f (Exponent a b) = f (Exponent (walk f a) (walk f b))
 walk f t@(Const{}) = f t
 walk f t@(Var{}) = f t
 
 data Matcher =
   RootMatcher
   | LeftMatcher Term
-  | SeriesMatcher
+  | AllMatcher
+  | SeriesMatcher String
 
-modify :: Matcher -> (Term -> Either Term Term) -> Term -> Either Term Term
-modify (LeftMatcher x) f t@(Sum a b) | x == a = f t
-modify (LeftMatcher x) f t@(Product a b) | x == a = f t
-modify RootMatcher f t = f t
-modify SeriesMatcher f t@(Series{}) = f t
-modify m f (Sum a b) =
-  let
-    rhs = modify m f a
-    lhs = modify m f b
-  in
-    Sum <$> rhs <*> lhs
-modify m f (Product a b) =
-  let
-    rhs = modify m f a
-    lhs = modify m f b
-  in
-    Product <$> rhs <*> lhs
-modify m f (Const a) = Right (Const a)
-modify m f t = Right t
+walkMatched :: Matcher -> (Term -> Either Term Term) -> Term -> Either Term Term
+walkMatched m f t = Right $ walk f' t
+  where
+    f' :: Term -> Term
+    f' t = if matcherApplies m t then
+             case f t of
+               Right t' -> t'
+               Left t' -> t'
+           else
+             t
+
+matcherApplies :: Matcher -> Term -> Bool
+matcherApplies (LeftMatcher x) (Sum a _) = x == a
+matcherApplies (LeftMatcher x) (Product a _) = x == a
+matcherApplies (SeriesMatcher v) (Series v' _ _) = v == v'
+matcherApplies AllMatcher _ = True
+matcherApplies _ _ = False
+
+--modify :: Matcher -> (Term -> Either Term Term) -> Term -> Either Term Term
+--modify (LeftMatcher x) f t@(Sum a b) | x == a = f t
+--modify (LeftMatcher x) f t@(Product a b) | x == a = f t
+--modify RootMatcher f t = f t
+--modify (SeriesMatcher v1) f (Series v2 i t) | v1 == v2 = (modify (SeriesMatcher v1) f t) >>= \inner -> f (Series v2 i inner)
+--modify m f (Sum a b) =
+--  let
+--    rhs = modify m f a
+--    lhs = modify m f b
+--  in
+--    Sum <$> rhs <*> lhs
+--modify m f (Product a b) =
+--  let
+--    rhs = modify m f a
+--    lhs = modify m f b
+--  in
+--    Product <$> rhs <*> lhs
+--modify m f (Const a) = Right (Const a)
+--modify m f t = Right t
 
 toAscii (Const a) = show a
 toAscii (Var a) = a
 toAscii (Sum a b) = "(" <> toAscii a <> " + " <> toAscii b <> ")"
-toAscii (Product a b) = "(" <> toAscii a <> " * " <> toAscii b <> ")"
+toAscii (Product a b) = "(" <> toAscii a <> "⋅" <> toAscii b <> ")"
 toAscii (Series v i t) = "Σ[" <> v <> " = " <> toAscii i <> "](" <> toAscii t <> ")"
+toAscii (Factorial t) = "(" <> toAscii t <> ")!"
+toAscii (Fraction a b) = "(" <> toAscii a <> "/" <> toAscii b <> ")"
+toAscii (Exponent a b) = toAscii a <> "^" <> toAscii b
 
 data Env = Env Term deriving (Show)
 
@@ -252,11 +311,12 @@ apply m axiom = do
   (Env t) <- get
 
   -- TODO: Handle error
-  case modify m (implementation axiom) t of
+  case walkMatched m (implementation axiom) t of
     Right t' -> do
       let t'' = walk (
                   ignoreError . implementation axiomSumConst .
-                  ignoreError . implementation axiomMultiplyConst
+                  ignoreError . implementation axiomMultiplyConst .
+                  ignoreError . implementation axiomNullExponent
                 ) t'
       tell [(t'', axiom)]
 
@@ -271,17 +331,32 @@ apply m axiom = do
 --  apply (LeftMatcher (Const 2)) axiomSumConst
 
 --body = runProcess (Series "k" (Const 0) (Sum (Var "x") (Var "k"))) $ do
---  apply SeriesMatcher axiomStepSeries
---  apply SeriesMatcher axiomStepSeries
+--  apply (SeriesMatcher "k") axiomStepSeries
+--  apply (SeriesMatcher "k") axiomStepSeries
 
 --body = runProcess (Product (Const 2) (Product (Const 3) (Const 4))) $ do
 --  apply RootMatcher axiomCommuteProduct
 --  apply (LeftMatcher (Const 3)) axiomMultiplyConst
 
-body = runProcess (Sum (Var "x") (Product (Var "x") (Var "x"))) $ do
-  apply RootMatcher axiomDistribute
-  apply RootMatcher axiomDistribute
+--body = runProcess (Sum (Var "x") (Product (Var "x") (Var "x"))) $ do
+--  apply RootMatcher axiomDistribute
+--  apply RootMatcher axiomDistribute
   --apply RootMatcher axiomDistribute
+
+-- TODO: Handle variable aliasing properly for nested series
+e_to t = (Series "k" (Const 0) (Fraction (Exponent t (Var "k")) (Factorial (Var "k"))))
+cos_x = (Series "m" (Const 0) (Product (Exponent (Const (-1)) (Var "m")) (Fraction (Exponent (Var "x") (Product (Const 2) (Var "m"))) (Factorial (Product (Const 2) (Var "m"))))))
+
+body = runProcess (e_to cos_x) $ do
+  apply (SeriesMatcher "m") axiomStepSeries
+  apply (SeriesMatcher "k") axiomStepSeries
+  apply AllMatcher axiomFactorialConst
+  apply AllMatcher axiomIdentityProduct
+  apply (SeriesMatcher "m") axiomStepSeries
+  apply (SeriesMatcher "k") axiomStepSeries
+  apply AllMatcher axiomFactorialConst
+  apply AllMatcher axiomIdentityProduct
+  --apply RootMatcher axiomAssociateSum
 
 printAxioms axioms = do
   let paddingIndex = length (show $ length axioms)
