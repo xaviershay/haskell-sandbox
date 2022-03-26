e1243a14db59af0b7db199212e8fedd4902b551b
Xavier Shay <contact@xaviershay.com>
2022-03-26 11:49:53 +1100

    Tidying and second example

--- app/LSystem.hs
+++ app/LSystem.hs
@@ -1,23 +1,14 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
 import Test.Tasty
 import Test.Tasty.HUnit
-import Data.String( IsString(..) )
 
 data Letter = Letter {
-  symbol :: Char
+  symbol :: String
 } deriving (Eq)
 
 instance Show Letter where
-  show (Letter x) = [x]
+  show (Letter x) = x
 
 type LWord = [Letter]
 
@@ -26,22 +17,7 @@ data Production = Production {
   replacement :: LWord
 } deriving (Show)
 
-lword = map Letter
-
-a = Letter 'A'
-b = Letter 'B'
-
-rules = [
-    Production {
-      matchSymbol = Letter 'B',
-      replacement = [a]
-    },
-    Production {
-      matchSymbol = Letter 'A',
-      replacement = [a, b]
-    }
-  ]
-
+lword = map Letter . words
 
 matchProduction :: [Production] -> Letter -> Production
 matchProduction ps l =
@@ -55,15 +31,25 @@ stepN 0 axiom _ = axiom
 stepN n axiom rules = stepN (n - 1) (step axiom rules) rules
 
 main = defaultMain tests
--- putStrLn . show $ stepN 4 [a] rules
 
-mkProductions :: [(Char, String)] -> [Production]
-mkProductions template = map (\(l, w) -> Production { matchSymbol = Letter l, replacement = lword w }) template
+mkProductions :: [(String, String)] -> [Production]
+mkProductions template = map (\(l, w) -> Production {
+  matchSymbol = Letter l,
+  replacement = lword w
+}) template
 
-tests = testGroup "LSystem"
-  [ testCase "DOL trivial" $ (lword "ababaaba") @=? (stepN 5 (lword "b")
+tests = testGroup "Deterministic & Context-Free (DOL)"
+  [ testCase "Trivial" $ (lword "a b a b a a b a") @=? (stepN 5 (lword "b")
       $ mkProductions [
-        ('b', "a"),
-        ('a', "ba")
+        ("b", "a"),
+        ("a", "b a")
       ])
+  , testCase "Anabaena catenula" $ (lword "bl ar bl ar ar bl ar ar") @=?
+      (stepN 4 (lword "ar")
+        $ mkProductions [
+          ("ar", "al br"),
+          ("al", "bl ar"),
+          ("br", "ar"),
+          ("bl", "al")
+        ])
   ]
