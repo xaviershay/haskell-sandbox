aa3ced118544b9006a7aae99dad1013393a1f32c
Xavier Shay <contact@xaviershay.com>
2023-12-06 18:02:07 +1100

    Day 2, Part 2

--- app/Aoc2023.hs
+++ app/Aoc2023.hs
@@ -7,7 +7,7 @@ module Main where
 import Debug.Trace
 import Data.Char (isDigit)
 import Data.List
-import Data.HashMap.Strict hiding (map, filter)
+import qualified Data.HashMap.Strict as M
 import Control.Arrow (first, (&&&))
 
 import Text.Parsec
@@ -27,31 +27,53 @@ parseUnsafe parser input =
     Right x -> x
     Left x -> error $ show x
 
-type Bag = HashMap String Int
+type Bag = M.HashMap String Int
 
 day2 = testGroup "Day 2"
   [ testCase "Example" $ do
-        x <- process "data/aoc2023/day2/example.txt"
+        x <- calculateValid "data/aoc2023/day2/example.txt"
         x @?= 8
   , testCase "Input" $ do
-        x <- process "data/aoc2023/day2/input.txt"
+        x <- calculateValid "data/aoc2023/day2/input.txt"
         x @?= 2512
+  , testCase "Part 2 Example" $ do
+        x <- calculatePower "data/aoc2023/day2/example.txt"
+        x @?= 2286
+  , testCase "Part 2 Input" $ do
+        x <- calculatePower "data/aoc2023/day2/input.txt"
+        x @?= 67335
   ]
   where
-    process filename =
+    calculatePower filename =
+      sum
+      . map power
+      . map (parseUnsafe game)
+      . lines <$> readFile filename
+      
+    calculateValid filename =
       sum
       . map fst
       . filter (validForBag $
-          fromList [("red", 12), ("green", 13), ("blue", 14)])
+          M.fromList [("red", 12), ("green", 13), ("blue", 14)])
       . map (parseUnsafe game)
       . lines <$> readFile filename
 
     validForBag :: Bag -> (Int, [Bag]) -> Bool
     validForBag bag (_, draws) =
       all (all (>= 0))
-      . map elems
-      . map (\draw -> unionWith (-) bag draw)
+      . map M.elems
+      . map (\draw -> M.unionWith (-) bag draw)
       $ draws
+
+    power :: (Int, [Bag]) -> Int
+    power (_, draws) =
+      let minBag = foldl'
+                     (\m x -> M.unionWith max m x)
+                     (M.fromList [("red", 0), ("green", 0), ("blue", 0)]) 
+                     draws
+      in
+
+      foldl' (*) 1 . M.elems $ minBag
 
     -- Parsers
     colorResult = do
@@ -61,7 +83,7 @@ day2 = testGroup "Day 2"
 
       return (color, n)
 
-    draw = fromList <$> colorResult `sepBy1` (string ", ")
+    draw = M.fromList <$> colorResult `sepBy1` (string ", ")
 
     game = do
        string "Game "
